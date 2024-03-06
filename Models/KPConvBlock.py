97ab69f5620519812df6c1adcd7c8612630be625
Tuyet Han <tuyethan97@gmail.com>
2024-03-06 10:56:23 +0700

    Grid Sample and KPConv Block

---
+++ Models/KPConvBlock.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+from torch_points3d.modules.KPConv.kernels import KPConvLayer
+from torch_points3d.core.common_modules import FastBatchNorm1d
+
+class KPConvSimpleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, prev_grid_size, sigma=1.0, negative_slope=0.2, bn_momentum=0.02):
+        super().__init__()
+        self.kpconv = KPConvLayer(in_channels, out_channels, point_influence=prev_grid_size * sigma, add_one=False)
+        self.bn = FastBatchNorm1d(out_channels, momentum=bn_momentum)
+        self.activation = nn.LeakyReLU(negative_slope=negative_slope)
+
+    def forward(self, feats, xyz, batch, neighbor_idx):
+        # feats: [N, C]
+        # xyz: [N, 3]
+        # batch: [N,]
+        # neighbor_idx: [N, M]
+
+        """
+        - query_points(torch Tensor): query of size N x 3
+        - support_points(torch Tensor): support points of size N0 x 3
+        - neighbors(torch Tensor): neighbors of size N x M
+        - features : feature of size N0 x d (d is the number of inputs)
+        """
+        feats = self.kpconv(xyz, xyz, neighbor_idx, feats)
+        feats = self.activation(self.bn(feats))
+        return feats
